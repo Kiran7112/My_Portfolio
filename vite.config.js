d369b8c95d0d671187e8ce862cb08a73d8df5c2c
Kiran7112 <maddireddykirankumar7112@gmail.com>
2025-02-26 22:48:28 +0530

    icons error

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -7,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@mui/icons-material'], // Add this line
+      // Remove the external option for @mui/icons-material
     },
   },
 });
