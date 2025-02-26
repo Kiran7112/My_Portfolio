45bbdd2e77a7aef15ed6212ac37486fbce6c63d7
Kiran7112 <maddireddykirankumar7112@gmail.com>
2025-02-26 21:57:08 +0530

    added all files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   base: "/",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['@mui/icons-material'], // Add this line
+    },
+  },
+});
